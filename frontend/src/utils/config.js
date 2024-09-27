a4b8801f90952937e4f7bc7734006e65047f84ac
ykp-narola <ykp@narola.email>
2024-09-27 18:12:55 +0530

    add songs

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,5 @@
 export const base = {
 	URL: import.meta.env.VITE_API_END_POINT, // API endpoint
-	NODE_ENV: import.meta.env.VITE_API_NODE_ENV === "development"
+	NODE_ENV: import.meta.env.VITE_API_NODE_ENV === "development",
+	RESOURCE: import.meta.env.VITE_API_RESOURCE
 };
