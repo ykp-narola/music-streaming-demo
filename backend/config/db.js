5ee7d9f5e833eca583b8a93f14af3172310c5826
ykp-narola <ykp@narola.email>
2024-09-27 12:20:38 +0530

    add console

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log('process.env.MONGO_CONNECTION :>> ', process.env.MONGO_CONNECTION); 
     await mongoose.connect(process.env.MONGO_CONNECTION);
     console.log('MongoDB connected');
   } catch (error) {
